bd45add326d0482517d63aeef7b0dd4f98e2faaa
zinzindev <zinzin30@windowslive.com>
2024-10-21 01:39:29 +0700

    update api

---
+++ src/app.js
@@ -0,0 +1,29 @@
+import 'dotenv/config';
+import express from 'express';
+import cors from 'cors';
+import helmet from 'helmet';
+import morgan from 'morgan';
+import chalk from 'chalk';
+
+import todoRoute from './routes/todo.js';
+import notFoundMiddleware from './middlewares/not-found.js';
+import errorMiddleware from './middlewares/error.js';
+
+const app = express();
+
+if (process.env.MODE === 'development') {
+	app.use(morgan('dev'));
+}
+
+app.use(cors());
+app.use(helmet());
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.use('/todos', todoRoute);
+
+app.use(notFoundMiddleware);
+app.use(errorMiddleware);
+
+const port = process.env.PORT || 8080;
+app.listen(port, () => console.log(chalk.blueBright(`server running on port ${port}`)));
